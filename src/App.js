e3b81bcf7bb17c4481b857a9f65e05e1d96f0fdd
JaeranKIM <devjaeran@gmail.com>
2023-08-07 15:28:00 +0900

    App.js JSX {}

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let post = "성수 맛집";
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4>BLOG</h4>
       </div>
+      <h4>{post}</h4>
     </div>
   );
 }
