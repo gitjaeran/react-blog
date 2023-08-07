d95e9385835a61d091680eca6c3b9768da8dd85f
JaeranKIM <devjaeran@gmail.com>
2023-08-07 15:29:57 +0900

    App.js JSX {} 데이터 바인딩

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>BLOG</h4>
+        <h4 id={post}>BLOG</h4>
       </div>
       <h4>{post}</h4>
     </div>
